057b6eec83faac8b222736818137b7de799655c0
Cornel-Adrian <co96grig@gmail.com>
2023-05-04 21:42:06 +0300

    Getting Token

---
+++ src/context/AuthProvider.tsx
@@ -0,0 +1,20 @@
+import React, { useContext, createContext, FC, useState, ReactNode } from 'react'
+
+interface Props {
+    children?: ReactNode
+}
+
+
+const AuthContext = createContext({});
+
+export const AuthProvider = ({ children, ...props }: Props) => {
+    const [auth, setAuth] = useState({...props});
+
+    return (
+        <AuthContext.Provider value={{ auth, setAuth }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthContext;
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -2,37 +2,18 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import './Login.css';
-import { useEffect, useState } from 'react';
-import axios from '../api/axios';
+import { useState } from 'react';
 
 
-function Login() {
+function Register() {
 
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
 
-    const handleLogin = async () => {
-
-        console.log("Ce plm")
-        await axios.post("/auth/login", { 'email': email, 'password': password }).then((res) => {
-            console.log(res.data.accessToken);
-        }).catch((err) => {
-            console.log("Error:" + err);
-        });
-
-    }
-
-    useEffect(() => {
-
-
-    }, [])
-
-
-
     return (
-        <div className="login">
+        <div className="register">
             <Box
                 component="form"
                 sx={{
@@ -46,21 +27,23 @@ function Login() {
                         required
                         id="outlined-required"
                         label="Email"
+                        defaultValue="Email"
+                        value={email}
                         onChange={(e) => setEmail(e.target.value)}
                     />
                     <TextField
-                        required
                         id="outlined-password-input"
                         label="Password"
                         type="password"
                         autoComplete="current-password"
+                        value={password}
                         onChange={(e) => setPassword(e.target.value)}
                     />
-                    <Button sx={{ mx: 'auto', background: '#2c5d4f' }} onClick={handleLogin} variant="contained" size="medium">Login</Button>
+                    <Button sx={{ mx: 'auto', background: '#2c5d4f' }} type='submit' variant="contained" size="medium">Register</Button>
                 </div>
             </Box >
         </div >)
 }
 
 
-export default Login;
+export default Register;
