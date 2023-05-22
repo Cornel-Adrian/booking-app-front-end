f44a80409f3ba4368fe01053e778ebf2915b3421
Cornel-Adrian <co96grig@gmail.com>
2023-05-22 18:25:12 +0300

    Update visual

--- src/components/TabelReviews.tsx
+++ src/components/TabelReviews.tsx
@@ -32,20 +32,22 @@ function TabelReviews() {
     return (
         <Container sx={{ marginTop: "50px" }}>
             <Typography variant="h4">Review-uri</Typography>
-            <List sx={{ display: 'flex', flexDirection: 'column' }}>
+            <List sx={{ display: 'flex', flexDirection: 'column', justifyContent: "space-evenly" }}>
                 {reviews ? reviews.map((review) => (
-                    <ListItem>
-                        <Box textAlign="center">
-                            <ListItemText key={review.reviewId} primary={review.name}
-                                sx={{ m:"20px" }}
-                            >
-                            </ListItemText>
-                        </Box>
-                        <Box>
-                            <Typography variant="body2">{review.message}</Typography>
-                            <Rating sx={{ m: 2 }} name="read-only" value={review.rating} readOnly />
-                        </Box>
-                    </ListItem>
+                    <Box sx={{alignSelf:'center'}}>
+                        <ListItem sx={{display:'block'}}>
+                            <Box textAlign="left">
+                                <ListItemText key={review.reviewId} primary={review.name}
+                                    sx={{ m: "20px", mx: 'auto' }}
+                                >
+                                </ListItemText>
+                            </Box>
+                            <Box>
+                                <Typography variant="body2">{review.message}</Typography>
+                                <Rating sx={{ m: 2 }} name="read-only" value={review.rating} readOnly />
+                            </Box>
+                        </ListItem>
+                    </Box>
                 )) : <p>Nu exista Reviewuri deocamdata</p>}
             </List>
         </Container >
